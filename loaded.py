660d7a8df205175a784adcf68015a1dfc7933ae2
Jono Pike <43720545+Soltis12@users.noreply.github.com>
2022-09-24 16:51:06 +0100

    Update loaded.py
    First build

--- loaded.py
+++ loaded.py
@@ -1,1 +1,24 @@
-streamlit.header('Loaded')
+# Show text for food choices
+import streamlit
+streamlit.title('Loaded')
+
+streamlit.header('Proper Dirty, Proper Epic')
+
+streamlit.text('🐖 Pulled Pork, Caramelised Onions, Cayenne Pepper and Tabasco Sauce')
+streamlit.text('🍗 Chicken, Chicken Skins, Cheddar Cheese and Barbeque Sauce')
+streamlit.text('🥑 Avocado, Free-From Cheese, Lettuce and Vegan Sauce')
+streamlit.text('🦆 Duck, Mushrooms, Bean Sprouts and Hoisin Sauce')
+
+streamlit.header('🏗️ Build your own Fruit Smoothie')
+
+# Import Fruit list
+#import pandas
+#my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+#my_fruit_list = my_fruit_list.set_index('Fruit')
+
+# Pick fruit
+#fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Lemon','Kiwifruit'])
+#fruits_to_show = my_fruit_list.loc[fruits_selected]
+
+# Display list of fruits
+#streamlit.dataframe(fruits_to_show)
